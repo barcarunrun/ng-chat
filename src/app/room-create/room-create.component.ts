da2031329447fda38d0006adf46d4ac683823867
ErgoFriend <erg.friend@gmail.com>
2019-09-09 13:55:35 +0900

    using sass

--- src/app/room-create/room-create.component.ts
+++ src/app/room-create/room-create.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from "@angular/core";
 
 @Component({
-  selector: 'app-room-create',
-  templateUrl: './room-create.component.html',
-  styleUrls: ['./room-create.component.css']
+  selector: "app-room-create",
+  templateUrl: "./room-create.component.html",
+  styleUrls: ["./room-create.component.sass"]
 })
 export class RoomCreateComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
