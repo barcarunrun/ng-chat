ad3b4338ecb5caafd5481b17d8309cd9bf4283cc
ErgoFriend <erg.friend@gmail.com>
2019-09-11 14:01:50 +0900

    homeコンポーネントを追加

---
+++ src/app/room-home/room-home.component.ts
@@ -0,0 +1,12 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: "app-room-home",
+  templateUrl: "./room-home.component.html",
+  styleUrls: ["./room-home.component.sass"]
+})
+export class RoomHomeComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
