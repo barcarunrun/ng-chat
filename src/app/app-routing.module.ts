95340ab78eeeef10f5d59d1047d4a03f45b001fd
ErgoFriend <erg.friend@gmail.com>
2019-10-19 16:24:50 +0900

    rm components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import {NgModule} from "@angular/core";
+import {Routes, RouterModule} from "@angular/router";
 // 追加
-import { LoginComponent } from "./login/login.component";
-import { AuthGuard } from "./auth/auth.guard";
-import { RoomDetailComponent } from "./room-detail/room-detail.component";
-import { RoomInviteListComponent } from "./room/invite/list/list.component";
-import { RoomHomeComponent } from "./room-home/room-home.component";
-import { AdminLayoutComponent } from "./layouts/admin-layout/admin-layout.component";
-import { AdminLayoutCompanyComponent } from "./layouts/admin-company-layout/admin-company-layout.component";
-import { RoomMainComponent } from "./layouts/room-layout/room-layout.component";
-import { DefaultComponent } from "./layouts/default-layout/default-layout.component";
+import {LoginComponent} from "./login/login.component";
+import {AuthGuard} from "./auth/auth.guard";
+import {RoomDetailComponent} from "./room-detail/room-detail.component";
+import {RoomInviteListComponent} from "./room/invite/list/list.component";
+import {RoomHomeComponent} from "./room-home/room-home.component";
+import {AdminLayoutComponent} from "./layouts/admin-layout/admin-layout.component";
+import {AdminLayoutCompanyComponent} from "./layouts/admin-company-layout/admin-company-layout.component";
+import {RoomMainComponent} from "./layouts/room-layout/room-layout.component";
+import {DefaultComponent} from "./layouts/default-layout/default-layout.component";
 
-import { UserComponent } from "./applicantAdmin/user/user.component";
+import {UserComponent} from "./applicantAdmin/user/user.component";
 
-import { CompanyComponent } from "./companyAdmin/user/user.component";
-import { TableComponent } from "./companyAdmin/table/table.component";
-import { EditComponent } from "./companyAdmin/edit/edit.component";
-import { UserlistComponent } from "./companyAdmin/userlist/userlist.component";
+import {CompanyComponent} from "./companyAdmin/user/user.component";
+import {TableComponent} from "./companyAdmin/table/table.component";
+import {EditComponent} from "./companyAdmin/edit/edit.component";
+import {UserlistComponent} from "./companyAdmin/userlist/userlist.component";
 
-import { IndexComponent } from "./index/index/index.component";
-import { DetailComponent } from "./index/detail/detail.component";
+import {IndexComponent} from "./index/index/index.component";
+import {DetailComponent} from "./index/detail/detail.component";
 
 const routes: Routes = [
   //  { path: "", redirectTo: "home", pathMatch: "full", canActivate: [AuthGuard] },
