762f1b6f2402ec7c7522ef32722f10eccb6b713f
ErgoFriend <erg.friend@gmail.com>
2019-11-01 21:38:47 +0900

    add logic

--- src/app/room/user-modal/user-modal.component.ts
+++ src/app/room/user-modal/user-modal.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from "@angular/core";
+import {MyAPIService} from "../../API.my";
 
 @Component({
-  selector: 'app-user-modal',
-  templateUrl: './user-modal.component.html',
-  styleUrls: ['./user-modal.component.css']
+  selector: "app-user-modal",
+  templateUrl: "./user-modal.component.html",
+  styleUrls: ["./user-modal.component.css"]
 })
 export class UserModalComponent implements OnInit {
+  @Input() username: string;
+  @Input() src: string;
+  isOpen: boolean;
+  user: any;
 
-  constructor() { }
+  constructor(private api: MyAPIService) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  toggleModal(): void {
+    this.isOpen = !this.isOpen;
+    if (this.isOpen) {
+      this.user = this.api.GetUser(this.username);
+      console.log("user modal:", this.user);
+    }
   }
-
 }
