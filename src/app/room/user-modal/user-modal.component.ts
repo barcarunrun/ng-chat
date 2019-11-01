2f439a818f4318c363079494c12746ad224ea6c2
ErgoFriend <erg.friend@gmail.com>
2019-11-01 23:48:12 +0900

    fix

--- src/app/room/user-modal/user-modal.component.ts
+++ src/app/room/user-modal/user-modal.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit, Input} from "@angular/core";
 import {MyAPIService} from "../../API.my";
+import {Auth, Storage} from "aws-amplify";
 
 @Component({
   selector: "app-user-modal",
@@ -10,17 +11,54 @@ export class UserModalComponent implements OnInit {
   @Input() username: string;
   @Input() src: string;
   isOpen: boolean;
-  user: any;
+  applicantName: string;
+  lastName: string;
+  firstName: string;
+  applicantId: string;
+  applicantAbout: string;
+  applicantEmail: string;
+  filename: string;
+  fileNameProfile: string;
+  fileNameBackground: string;
+  fileUrlProfile: Object | String;
+  fileUrlBackground: Object | String;
 
-  constructor(private api: MyAPIService) {}
+  constructor(private api: MyAPIService) {
+    this.api.GetUser(this.username).then(user => {
+      this.filename = user.id + ".png";
+    });
+  }
 
   ngOnInit() {}
 
   toggleModal(): void {
     this.isOpen = !this.isOpen;
     if (this.isOpen) {
-      this.user = this.api.GetUser(this.username);
-      console.log("user modal:", this.user);
+      this.api.MyGetApplicant(this.username).then(data => {
+        // console.log(data);
+        this.applicantName = data.lastName + " " + data.firstName;
+        this.applicantId = data.id;
+        this.applicantAbout = data.about;
+        this.applicantEmail = data.email;
+        this.lastName = data.lastName;
+        this.firstName = data.firstName;
+
+        this.fileNameBackground = "student/background/" + this.filename;
+        this.fileNameProfile = "student/profile/" + this.filename;
+
+        Storage.get(this.fileNameBackground)
+          .then(result => {
+            console.log(result);
+            this.fileUrlBackground = result;
+          })
+          .catch(err => console.log(err));
+        Storage.get(this.fileNameProfile)
+          .then(result => {
+            console.log(result);
+            this.fileUrlProfile = result;
+          })
+          .catch(err => console.log(err));
+      });
     }
   }
 }
