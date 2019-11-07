c9d34a26cf1d75b3c475017cc0b449e0db689656
shiba72 <omen2412@gmail.com>
2019-11-07 16:49:25 +0900

    landingpage update

--- src/app/index/landingpage/landingpage.component.ts
+++ src/app/index/landingpage/landingpage.component.ts
@@ -2,15 +2,19 @@ import { Component, OnInit, QueryList } from "@angular/core";
 import Amplify, { Auth, Hub, graphqlOperation, Storage } from "aws-amplify";
 import { MyAPIService } from "../../API.my";
 
-import {} from "../../API.service";
+import { } from "../../API.service";
 
 @Component({
   selector: "landingpage-cmp",
   moduleId: module.id,
-  templateUrl: "landingpage.component.html"
+  templateUrl: "landingpage.component.html",
+  styleUrls: ['css/bootstrap.min.css',
+    'css/jquery.fancybox.min.css', 'css/bootstrap-datepicker.css',
+    'css/aos.css', 'css/style.css', 'fonts/icomoon/style.css', 'fonts/flaticon/font/flaticon.css',
+    'css/jquery-ui.css', 'css/owl.theme.default.min.css', 'css/owl.carousel.min.css'],
 })
 export class LandingPageComponent implements OnInit {
-  async ngOnInit() {}
+  async ngOnInit() { }
 
   redirect(id) {
     location.href = "/detail?id=" + id;
